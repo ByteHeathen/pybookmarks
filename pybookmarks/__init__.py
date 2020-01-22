626d15f5190457e89af88cb8cbe21868c0c80812
Bytebuddha <pmg41494@protonmail.com>
2020-01-21 21:17:07 -0600

    add root_folder function

--- pybookmarks/__init__.py
+++ pybookmarks/__init__.py
@@ -1,3 +1,4 @@
 from .pybookmarks import Tag
 from .pybookmarks import BookMark
 from .pybookmarks import Folder
+from .pybookmarks import root_folders
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 
 mod tag;
 pub use tag::Tag;
@@ -10,11 +11,27 @@ pub use folder::Folder;
 mod bookmark;
 pub use bookmark::BookMark;
 
+#[pyfunction]
+/// List all Folders with no parent folder.
+fn root_folders(database_path: Option<String>) -> PyResult<Vec<Folder>> {
+    use libbookmarks::BookMarksApi;
+
+    let api = BookMarksApi::new(database_path)?;
+    Ok(api.root_folders()?.iter().map(|item| {
+        Folder {
+            id: item.id,
+            label: item.label.clone(),
+            parent: item.parent
+        }
+    }).collect())
+}
+
 /// libbookmarks is a Rust library for managing bookmarks statically.
 #[pymodule]
 fn pybookmarks(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Tag>()?;
     m.add_class::<Folder>()?;
     m.add_class::<BookMark>()?;
+    m.add_wrapped(wrap_pyfunction!(root_folders))?;
     Ok(())
 }
